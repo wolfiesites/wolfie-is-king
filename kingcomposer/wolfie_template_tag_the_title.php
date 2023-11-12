6923a1001d3d1ec3ae628a3e081af0b5ed5b0e1f
Paweł Witek <wit.paw4@gmail.com>
2023-11-12 19:02:04 +0100

    fixed the title template tag. now it is properly styled

--- kingcomposer/wolfie_template_tag_the_title.php
+++ kingcomposer/wolfie_template_tag_the_title.php
@@ -1,14 +1,16 @@
 <?php
+
 $text	= $post_title = $type = $before = $after = $title_link = $link_url = $link_title = $link_target = $class = $title_wrap = $title_wrap_class = '';
 
 extract( $atts );
+
 if( empty( $type ) )
 	$type = 'h1';
 
 $wrap_class  = apply_filters( 'kc-el-class', $atts );
-$class_title = array( 'wolfie_title' );
+$class_title = array( 'kc_title' );
 
-$wrap_class[] = 'wolfie-title-wrap';
+$wrap_class[] = 'kc-title-wrap';
 
 if ( !empty( $class ) )
 	$class_title[] = $class;
@@ -29,12 +31,13 @@ if ( !empty( $title_link ) ) {
 		$link_target = $link_arr[2];
 
 }
+	
+	$text_title = get_the_title();
+	
+	if( $text_title != '' )
+		$text = esc_attr($text_title);
+	
 
-$text_title = get_the_title();
-	
-if( $text_title != '' )
-	$text = esc_attr($text_title);
-	
 	
 $wrap_class[] = $title_wrap_class;
 
@@ -48,7 +51,7 @@ $wrap_class[] = $title_wrap_class;
 
 		echo '<'. $type .' class="'. implode( ' ', $class_title ) . '">';
 			if ( !empty( $link_url ) ) {
-				echo '<a href="'. esc_url( $link_url ) .'" class="wolfie_title_link" title="'. esc_attr( $link_title ) .'" target="'. esc_attr( $link_target ) .'">'. $text .'</a>';
+				echo '<a href="'. esc_url( $link_url ) .'" class="kc_title_link" title="'. esc_attr( $link_title ) .'" target="'. esc_attr( $link_target ) .'">'. $text .'</a>';
 			} else {
 				echo $text;
 			}
